3f5af6c005842156d01bbea7d281372511a592af
lairdstewart <62522578+lairdstewart@users.noreply.github.com>
2026-01-05 11:48:12 -0500

    Update init2.lua
    cygwin windows support

--- init2.lua
+++ init2.lua
@@ -1,3 +1,15 @@
+if vim.fn.has("win32") == 1 then
+    vim.opt.shell = "C:/cygwin64/bin/bash.exe"
+    vim.opt.shellcmdflag = "-c"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+    vim.opt.shellslash = true
+    vim.g.netrw_localmovecmd = "mv"
+    vim.g.netrw_localcopycmd = "cp"
+    vim.g.netrw_localmkdir = "mkdir"
+    vim.g.netrw_browsex_viewer = "cygstart"
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
